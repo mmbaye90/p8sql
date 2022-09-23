34cd67e2950278bb06d104116f78a124a971b7d5
Mbaye <mmbaye1@hotmail.fr>
2022-09-23 08:37:08 +0200

    Ajout des variables d'environnements sur le fichier config du DB

--- back/config/db.js
+++ back/config/db.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql");
+require("dotenv").config();
 
 // Mise place de la connexion en Local
 const db = mysql.createConnection({
-    host: "localhost",
-    username: "root",
-    password: "azerty",
-    database: "groupomania",
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 });
 db.connect(function(err) {
     if (err) throw err;

00aace8cd7a7f645962e38dec91ecb65b1b2e167
Mbaye <mmbaye1@hotmail.fr>
2022-09-28 22:40:00 +0200

    Configuration pour le déploiement sur Heroku

--- back/config/db.js
+++ back/config/db.js
@@ -2,12 +2,17 @@ const mysql = require("mysql");
 require("dotenv").config();
 
 // Mise place de la connexion en Local
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-});
+let db;
+if (process.env.CLEARDB_DATABASE_URL) {
+    db.createConnection(CLEARDB_DATABASE_URL);
+} else {
+    db = mysql.createConnection({
+        host: process.env.DB_HOST,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+    });
+}
 db.connect(function(err) {
     if (err) throw err;
     console.log("CONNEXION  à la BD REUSSIE");
